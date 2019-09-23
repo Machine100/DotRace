7cc1501c199f60899697e143d8d48d4dbe005826
Machine100 <joesmileyyy@yahoo.com>
2019-09-23 08:23:05 -0400

    .

--- src/app/dotcontrol/dotcontrol.service.ts
+++ src/app/dotcontrol/dotcontrol.service.ts
@@ -11,7 +11,7 @@ export class DotcontrolService {
   private dot11 = new BehaviorSubject <Dot> ({row:1, column:1, owner:null})
   dot11$ = this.dot11.asObservable()           // This shares out to the view components
 
-  constructor(private db: AngularFirestore) { }
+  constructor(private db: AngularFirestore) { }     // error in here   <---------------
 
   claimDot(row: number, column: number, id: string) {
     console.log (row, column)
