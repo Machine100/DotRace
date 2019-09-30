6f347dd8ccace26a1610d4ca6411e2b86625c8f1
Machine100 <joesmileyyy@yahoo.com>
2019-09-30 14:38:42 -0400

    .

--- src/app/components/controlpanel/controlpanel.component.ts
+++ src/app/components/controlpanel/controlpanel.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DotcontrolService } from '../../dotcontrol/dotcontrol.service';
-//import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-controlpanel',
